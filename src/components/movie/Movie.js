d98204e4a6b2291c883b46703c8cedf27252307e
Bipul shukla <bipulshukla12@gmail.com>
2023-08-23 14:26:47 +0530

    hello

---
+++ src/components/movie/Movie.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./Movie.css"
+
+
+const Movie = () => {
+    return (
+        <div className="Movie">
+            Movie
+        </div>
+    )
+}
+
+export default Movie;
